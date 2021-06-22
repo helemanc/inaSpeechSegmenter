c6d297fbeae7097e9cc5e0a1c607a0d753220093
helemanc <elemanc@hotmail.it>
2021-06-22 13:40:24 +0200

    Added print pred in segmenter.py

--- inaSpeechSegmenter/segmenter.py
+++ inaSpeechSegmenter/segmenter.py
@@ -149,6 +149,7 @@ class DnnSegmenter:
             rawpred = rawpred[l:]
             r[finite[start:stop] == False, :] = 0.5
             pred = viterbi_decoding(np.log(r), diag_trans_exp(self.viterbi_arg, len(self.outlabels)))
+            print("Pred: ", pred)
             for lab2, start2, stop2 in _binidx2seglist(pred):
                 ret.append((self.outlabels[int(lab2)], start2+start, stop2+start))            
         return ret
